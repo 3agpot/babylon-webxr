42282bac1a86a355390581bcb13d1aa24bf271ef
Rashid <rashid.makhmud@gmail.com>
2021-03-19 10:06:15 -0500

    erroring: WebXRExperienceHelper is not defined

--- src/webxr.js
+++ src/webxr.js
@@ -4,6 +4,7 @@ import { Engine } from "@babylonjs/core/Engines/engine";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
 import { Vector3 } from "@babylonjs/core/Maths";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { PointLight } from "@babylonjs/core/Lights/pointLight";
 import { Mesh } from "@babylonjs/core/Meshes";
 import { Loaders } from "@babylonjs/loaders"; // TODO: figure out how to include without it being greyed out. We need this to load gltf and glb models.
 
@@ -29,14 +30,49 @@ var createScene = async function () {
     camera.attachControl(canvas, true);
     var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
+    let pointLight = new PointLight('light2', new Vector3(0.5, 5, 0.5), scene);
+    pointLight.intensity = 0.2;
     var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
     sphere.position.y = 1;
+    
 
     const env = scene.createDefaultEnvironment();
 
-    const xr = await scene.createDefaultXRExperienceAsync({
+    /*const xr = await scene.createDefaultXRExperienceAsync({
         floorMeshes: [env.ground]
-    });
+    });*/
+
+    try {
+        console.log("before");
+        const xrHelper = await WebXRExperienceHelper.CreateAsync(scene);
+        console.log("after");
+        xrHelper.onStateChangedObservable.add((state) => {
+            switch (state) {
+                case WebXRState.IN_XR:
+                    console.log("XR is initialized and already submitted one frame")
+                case WebXRState.ENTERING_XR:
+                    console.log("xr is being initialized, enter XR request was made");
+                case WebXRState.EXITING_XR:
+                    console.log("xr exit request was made. not yet done");
+                case WebXRState.NOT_IN_XR:
+                    console.log("self explanatory - either our or not yet in XR");
+            }
+        })
+        const sessionManager = await xrHelper.enterXRAsync("immersive-vr", "local-floor"); 
+        const supported = await sessionManager.isSessionSupportedAsync('immersive-vr');
+        
+        if (supported) {
+            console.log("xr available, session supported");
+            const xr = sessionManager.initializeSessionAsync('immersive-vr' /*, xrSessionInit */ );
+        }
+    } catch (e) {
+        console.log("no XR support");
+        console.error(e);
+    }
+
+    /*let cam = xr.input.xrCamera
+    console.log("logging camera");
+    console.log(cam);*/
 
     return scene;
 };
